7c4c2c8a80e1f3d52bbe3947f673535078122b1a
Sharipov Artur <Sharipov@dedicorp.ru>
2025-10-31 11:50:22 +0300

    add config_example.py and delete Dockerfile

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM ubuntu:latest
-RUN apt-get update
-RUN apt-get install -y python3
-RUN apt-get install -y python3-pip
-WORKDIR /app
-COPY . /app/
-RUN pip install -r requirements.txt --break-system-packages
---
+++ config_example.py
@@ -0,0 +1,14 @@
+prod_bot_token = ""
+test_bot_token = ""
+database_path = "blabla_bot_db.sql"
+admin_tg_id = XXXX
+
+spreadsheet_id = ''
+creds_json = 'blabla-bar-420906-ef5facd3f81b.json'
+
+max_person_cnt = 8
+row_width = 3
+day_cnt = 7
+book_record_cnt = 6  # 6*30=3ч
+book_duration = 3 # в часах
+hall_img = "bla_bla_hall.jpg"
